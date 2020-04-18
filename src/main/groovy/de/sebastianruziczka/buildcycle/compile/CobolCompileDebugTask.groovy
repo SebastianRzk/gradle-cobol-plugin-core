14f3ead4266f1d6d0ed4d3d3e81d89844e1beb6d
RosesTheN00b <RosesTheN00b@github>
2020-04-18 14:07:54 +0200

    changed input resolving

--- src/main/groovy/de/sebastianruziczka/buildcycle/compile/CobolCompileDebugTask.groovy
+++ src/main/groovy/de/sebastianruziczka/buildcycle/compile/CobolCompileDebugTask.groovy
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileType
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
@@ -12,6 +13,7 @@ import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.SkipWhenEmpty
 import org.gradle.api.tasks.TaskAction
+import org.gradle.work.ChangeType
 import org.gradle.work.Incremental
 import org.gradle.work.InputChanges
 import de.sebastianruziczka.CobolExtension
