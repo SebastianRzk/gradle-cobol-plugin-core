b01773018efe969a3a9aed8f0b45cd98ef3746bf
RosesTheN00b <RosesTheN00b@github>
2020-04-18 13:51:43 +0200

    switch to list in incremental task input

--- src/main/groovy/de/sebastianruziczka/buildcycle/compile/CobolCompileDebugTask.groovy
+++ src/main/groovy/de/sebastianruziczka/buildcycle/compile/CobolCompileDebugTask.groovy
@@ -44,7 +44,7 @@ class CobolCompileDebugTask extends DefaultTask{
 		Set<String> done = new HashSet<>()
 		String sourceModule = this.configuration.projectFileResolver(this.configuration.srcMainPath).absolutePath
 		
-		inputs.getFileChanges([getFileChanges]).each { change ->
+		inputs.getFileChanges().each { change ->
 			if (change.fileType == FileType.DIRECTORY) return
 
 			println "${change.changeType}: ${change.normalizedPath}"
