1fa82c26fd95fa5d4c838a56a34b55bbb0a683f3
SebastianRzk <5215946+SebastianRzk@users.noreply.github.com>
2021-01-20 16:52:56 +0100

    Update gh-name

--- ci/endToEndTest.sh
+++ ci/endToEndTest.sh
@@ -1,6 +1,6 @@
 set -e
 
-git clone https://github.com/RosesTheN00b/gradle-cobol-plugin-example
+git clone https://github.com/SebastianRzk/gradle-cobol-plugin-example
 cd gradle-cobol-plugin-example
 sh ci/run_core_dev_test.sh
 cd ..
